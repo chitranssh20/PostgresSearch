da957c0fd16fa87806825390057838216976e767
chitranssh20 <saxenaayush2052001@gmail.com>
2022-11-06 15:48:01 +0530

    Undefined Psycopg2 error

--- fts/search/models.py
+++ fts/search/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title: models.CharField(max_length = 1000, db_index= True, null = False)
-    author: models.CharField(max_length= 5000)
+    title= models.CharField(max_length = 1000, db_index= True, null = False)
+    authors= models.CharField(max_length= 5000)
+
+
 
     
 #   class Meta:
